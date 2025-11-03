96f896cea16e4f698b476dc3e5273499d8b95a0b
murYG <mur@jinr.ru>
2025-11-03 18:21:54 +0300

    step 4

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -1,18 +1,48 @@
 <?php
 
-namespace Gendiff\Gendiff;
+namespace Differ\Differ;
+
+use Funct;
 
 function parse(string $filePath)
 {
     $file = file_get_contents($filePath);
-
     $result = json_decode($file);
 
-    return $result;
+    return get_object_vars($result);
 }
 
-function printData(\stdClass $data, $format)
+function genDiff(string $pathToFile1, string $pathToFile2): string
 {
-    echo "Формат: $format\n";
-    print_r($data);
+    $result1 = parse($pathToFile1);
+    $result2 = parse($pathToFile2);
+
+    $keys = array_unique(array_merge(array_keys($result1), array_keys($result2)));
+    $keys = Funct\Collection\sortBy($keys, fn ($value) => $value);
+
+    $arResult = array_reduce($keys, function ($acc, $key) use ($result1, $result2) {
+        if (!isset($result1[$key])) {
+            $acc[] = getDiffFormatted($key, $result2[$key], "+");
+        } elseif (!isset($result2[$key])) {
+            $acc[] = getDiffFormatted($key, $result1[$key], "-");
+        }
+
+        if (isset($result1[$key]) && isset($result2[$key])) {
+            if ($result1[$key] === $result2[$key]) {
+                $acc[] = getDiffFormatted($key, $result1[$key], " ");
+            } else {
+                $acc[] = getDiffFormatted($key, $result1[$key], "-");
+                $acc[] = getDiffFormatted($key, $result2[$key], "+");
+            }
+        }
+
+        return $acc;
+    }, []);
+
+    return "{\n" . implode("\n", $arResult) . "\n}\n";
+}
+function getDiffFormatted($key, $value, $sign)
+{
+    $value = var_export($value, true);
+    return "  $sign $key: $value";
 }
