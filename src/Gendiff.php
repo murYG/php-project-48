85b8992b2e786fccf9e5be22f2b1fe0cca5ee619
murYG <mur@jinr.ru>
2025-11-03 16:14:08 +0300

    step 3, lint rec

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -4,15 +4,15 @@ namespace Gendiff\Gendiff;
 
 function parse(string $filePath)
 {
-	$file = file_get_contents($filePath);
-	
-	$result = json_decode($file);
-	
-	return $result;
+    $file = file_get_contents($filePath);
+
+    $result = json_decode($file);
+
+    return $result;
 }
 
 function printData(\stdClass $data, $format)
 {
-	echo "Формат: $format\n";
-	print_r($data);
+    echo "Формат: $format\n";
+    print_r($data);
 }
