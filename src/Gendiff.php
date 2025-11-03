35a45f83f1eb0dde426f7d9687d41f5f57cba0a3
murYG <mur@jinr.ru>
2025-11-03 16:10:36 +0300

    step 3

---
+++ src/Gendiff.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Gendiff\Gendiff;
+
+function parse(string $filePath)
+{
+	$file = file_get_contents($filePath);
+	
+	$result = json_decode($file);
+	
+	return $result;
+}
+
+function printData(\stdClass $data, $format)
+{
+	echo "Формат: $format\n";
+	print_r($data);
+}
